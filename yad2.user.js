68ab764f8ef90ddced91a01a2a1696d6090d9181
Nitzans <nizan1212@gmail.com>
2021-07-10 12:50:22 +0300

    Trying to get extra info

--- yad2.user.js
+++ yad2.user.js
@@ -13,18 +13,31 @@
     var results = "";
     var cars = document.getElementsByClassName("feeditem table");
 	for (var i = 0; i < cars.length; i++){
-		var price = cars[i].getElementsByClassName("price")[0].innerText.replace(',', '').replace('₪', '');
+		cars[i].click();
         var model = cars[i].getElementsByClassName("title")[0].innerText;
         var how_old = cars[i].getElementsByClassName("middle_col")[0];
         var year = how_old.getElementsByClassName("data")[0].childNodes[0].innerText;
         var hand = how_old.getElementsByClassName("data")[1].childNodes[0].innerText;
+		var price = cars[i].getElementsByClassName("price")[0].innerText.replace(',', '').replace('₪', '');
 		if (price.includes("לא צוין") || price.includes("לחודש")){
             console.log("removed!!");
-            cars[i].style.visibility = "hidden";
+            //cars[i].style.visibility = "hidden";
 			continue;
         }
-
+		
+		
+		var more_details = cars[i].getElementsByClassName("details_wrapper")[0].childNodes;
+		var kilometer = more_details[0].childNodes[2].innerText;
+		var prev_owner = more_details[6].childNodes[2].innerText;
+		var curr_owner = more_details[7].childNodes[2].innerText;
+		console.log("kilometer: " + kilometer);
+		console.log("prev: " + prev_owner + " curr: " + curr_owner);
+		if (!prev_owner.includes("פרטית") && !curr_owner.includes("פרטית")){
+			console.log("BAD: " + prev_owner + "//" + curr_owner)
+			continue;
+		}
         results = results + model + ", " + price + ", " + year + ", " + hand + "\n";
+		cars[i].click();
 	}
 	console.log(results);
     console.log("Done");
